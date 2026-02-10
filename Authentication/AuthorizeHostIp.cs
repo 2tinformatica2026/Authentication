85b2d70483be5a5b5e6915988c913850a1cd46ae
Antonio <atorre@2tinformatica.it>
2026-02-10 13:13:05 +0100

    Create AuthorizeHostIp.cs

---
+++ Authentication/AuthorizeHostIp.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+namespace [Project name].UserAuthentication
+{
+    public class AuthorizeHostIp : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var RemoteIpAddress = context.HttpContext.Connection.RemoteIpAddress;
+            var hasClaim = context.HttpContext.User.Claims.Any(c => c.Type == abstractAuthentication.hostip && c.Value == (RemoteIpAddress == null?string.Empty: RemoteIpAddress.ToString()));
+            if (!hasClaim)
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+    }
+}
